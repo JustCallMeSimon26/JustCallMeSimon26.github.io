f1207cbfaa7f28e23a30f341df09323cfa692831
Simon <simon1240.se@gmail.com>
2022-02-01 01:26:21 +0100

    I dont really know what im doing

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Load Articles
 fetch("articles/articles.json")
 	.then(response => response.json())
-	.then(data => alert(data))
+	.then(data => console.log(data))
+
+// Get Article Count
+
+
+//get element to add stuff to
+const right_side = document.getElementsByClassName("right")
+
+// add this but with js
+/* <div class="article">
+				<div class="article-title">
+					<h2 class="article-title-text">
+						Lightshot Crawler
+					</h2>
+				</div>
+				<div class="article-thumbnail">
+					<img class="article-thumbnail-image" src="articles/res/Thumbnail.png" alt="Logo goes here">
+				</div>
+				<div class="article-description">
+					<h2 class="article-description-text">
+						Find Random Screenshots by guessing them
+					</h2>
+				</div>
+			</div> */
