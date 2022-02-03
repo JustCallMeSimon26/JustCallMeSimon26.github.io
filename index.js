4549bbb64e2825c6c5b422636092ed86e065d0da
Simon <simon1240.se@gmail.com>
2022-02-03 19:02:39 +0100

    Did stuff but it no want to work

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-// Load Articles
-fetch("articles/articles.json")
+function addArticles(path) {
+	fetch(path)
 	.then(response => response.json())
-	.then(data => console.log(data))
+	.then(data => {
+		const rightSide = document.getElementById('right')
+		for (let articleIndex = 0; articleIndex < data.length; articleIndex++) {
+		
+			const titleText = document.createTextNode(data[articleIndex]["title"])
+			
+			const articleTitle = document.createElement("h2")
+			articleTitle.appendChild(titleText)
+			articleTitle.classList.add('article-title-text')
+			
+			const articleTitleContainer = document.createElement("div")
+			articleTitleContainer.appendChild(articleTitle)
+			articleTitleContainer.classlist.add('article-title')
+			
+			const articleImage = document.createElement("img")
+			articleImage.src = data[articleIndex]["image"]
+			articleImage.alt = "Thumbnail goes here"
+			articleImage.classList.add('article-thumbnail-image')
 
-// Get Article Count
+			const articleImageContainer = document.createElement("div")
+			articleImageContainer.appendChild(articleImage)
+			articleImageContainer.classlist.add('article-thumbnail')
 
+			const descriptionText = document.createTextNode(data[articleIndex]["desc"])
+			
+			const articleDescription = document.createElement("h2")
+			articleDescription.appendChild(descriptionText)
+			articleDescription.classList.add('article-description-text')
 
-//get element to add stuff to
-const right_side = document.getElementsByClassName("right")
+			const articleDescriptionContainer = document.createElement("div")
+			articleDescriptionContainer.appendChild(articleDescription)
+			articleDescriptionContainer.classlist.add("article-description")
 
-// add this but with js
-/* <div class="article">
-				<div class="article-title">
-					<h2 class="article-title-text">
-						Lightshot Crawler
-					</h2>
-				</div>
-				<div class="article-thumbnail">
-					<img class="article-thumbnail-image" src="articles/res/Thumbnail.png" alt="Logo goes here">
-				</div>
-				<div class="article-description">
-					<h2 class="article-description-text">
-						Find Random Screenshots by guessing them
-					</h2>
-				</div>
-			</div> */
+			const articleContainer = document.createElement("div")
+			articleContainer.appendChild(articleTitleContainer)
+			articleContainer.appendChild(articleImageContainer)
+			articleContainer.appendChild(articleDescriptionContainer)
+			articleContainer.classlist.add('article')
+			
+			rightSide.appendChild(articleContainer)
+			}
+		}
+	)
+}
+
+const articlesPath = "articles/articles.json"
+addArticles(articlesPath)
